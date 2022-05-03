7889709f8295e5d42f3190d64dc89095b0bc8e26
Roger Almengor Gonzalez <roger.almengor@gaf.de>
2022-05-03 21:40:21 +0200

    2do: click on coockies

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
+from re import I
 from playwright.sync_api import sync_playwright
+import bs4
+URL = "https://duden.de"
 
 
 def main(): 
-    with sync_playwright() as p:
-        browser = p.chromium.launch()
-        page = browser.new_page()
-        page.goto("http://playwright.dev")
-        print(page.title())
-        browser.close()
+    with sync_playwright() as p: 
+        browser = p.chromium.launch(headless=False, slow_mo=50)
+        page = browser.new_page() 
+        page.goto(URL)
+        page.fill("input#edit-search-api-fulltext--2", "Pferd")
+        page.click('text=Naschlagen')
+        page.is_visible()
+        html = page.inner_html()
+        soup = bs4.BeautifulSoup(html, 'html.parser')
+        print(soup.find_all('strong'))
+
 
 if __name__ == "__main__":
     main()
