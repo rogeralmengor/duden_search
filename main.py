886f079db010ad7c14499de637d516cede581049
Roger Almengor Gonzalez <roger.almengor@gaf.de>
2022-05-05 10:49:34 +0200

    enumerate for loop

--- main.py
+++ main.py
@@ -10,34 +10,47 @@ def run(playwright: Playwright) -> None:
     page = context.new_page()
     print("Go to https://www.duden.de/")
     page.goto("https://www.duden.de/")
-    print("Click text=AKZEPTIEREN >> nth=0")
+    
+    print("Accept first pop up message")
     page.frame_locator("#sp_message_iframe_622759").locator("text=AKZEPTIEREN").first.click()
-    print("Click [placeholder=\"Stichwort\"]")
+    print("Place cursor on placeholder")
     page.locator("[placeholder=\"Stichwort\"]").click()
-    print("Press CapsLock")
     page.locator("[placeholder=\"Stichwort\"]").press("CapsLock")
-    print("Fill [placeholder=\"Stichwort\"]")
+    
+    print(f"Fill placeholder with {word}")
     page.locator("[placeholder=\"Stichwort\"]").fill("B")
-    print("Press CapsLock")
     page.locator("[placeholder=\"Stichwort\"]").press("CapsLock")
-    print("Fill [placeholder=\"Stichwort\"]")
     page.locator("[placeholder=\"Stichwort\"]").fill(word)
+    
     print("Click text=Nachschlagen oder Nachschlagen →")
     page.locator('button[class="form-asap__button"]').click()
+    
     print(f'Word:\"{word}\" nachschlagen')
     page.locator('text=Zum vollst').nth(0).click()
-    print("get html")
+    
+    print("get html with bs4")
     html = page.inner_html("div:has(div.division)")
     import re
     soup = bs4.BeautifulSoup(html, "html.parser")
     bedeutung = soup.find_all("div", {"class":"enumeration__text"})
-    print(type(bedeutung))
+
+    print("Getting definitions as list")
+    print("="*80)
     definitions = []
     for tag in bedeutung:
         definitions.append((tag.text.strip("\n")))
-    for definition in definitions:
-        print(definition)
+    #for definition in definitions:
+    #    print(definition)
+    for count, definition in enumerate(definitions):
+        print(count, definition)
+        print("-"*80)
+    
+    print("="*80)
+    
+    print("Closing everything")
     context.close()
     browser.close()
-with sync_playwright() as playwright:
-    run(playwright)
+
+if __name__ == "__main__": 
+    with sync_playwright() as playwright:
+        run(playwright)
