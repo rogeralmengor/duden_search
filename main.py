bb4b1398bdbb5efac8ad553695437fa431a68e1c
Roger Almengor Gonzalez <roger.almengor@gaf.de>
2022-05-10 08:20:29 +0200

    added logs, and removed re

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import json
 import pandas as pd
 import argparse
 import html_to_json
+import logging
 
 
 def parseArguments(): 
@@ -45,7 +46,6 @@ def store_html_as_json(url:str, json_path:str):
 
     with open(temp_dir_path + "/" + "foo_bar.html", "r", encoding="utf-8") as html_file:
         html = html_file.read()
-        print(html)
         json_ = html_to_json.convert(html)
     
     with open(json_path, "w", encoding="utf-8") as file:
@@ -57,39 +57,42 @@ def export_definitions_as_csv(csv_path: str, definitions:list, word:str):
 
 def run(playwright: Playwright, csv_path:str, json_path:str, word:str) -> None:
     
+    # Configuring logger 
+    
+    logging.basicConfig(filename='Prozess.log', encoding='utf-8', level=logging.DEBUG)
+    
     # Getting input arguments
 
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     page = context.new_page()
-    print("Go to https://www.duden.de/")
+    logging.debug("Go to https://www.duden.de/")
     page.goto("https://www.duden.de/")
     
-    print("Accept first pop up message")
+    logging.debug("Accept first pop up message")
     page.frame_locator("#sp_message_iframe_622759").locator("text=AKZEPTIEREN").first.click()
-    print("Place cursor on placeholder")
+    logging.debug("Place cursor on placeholder")
     page.locator("[placeholder=\"Stichwort\"]").click()
     page.locator("[placeholder=\"Stichwort\"]").press("CapsLock")
     
-    print(f"Fill placeholder with {word}")
+    logging.debug(f"Fill placeholder with {word}")
     page.locator("[placeholder=\"Stichwort\"]").fill("B")
     page.locator("[placeholder=\"Stichwort\"]").press("CapsLock")
     page.locator("[placeholder=\"Stichwort\"]").fill(word)
     
-    print("Click text=Nachschlagen oder Nachschlagen →")
+    logging.debug("Click text=Nachschlagen oder Nachschlagen →")
     page.locator('button[class="form-asap__button"]').click()
     
-    print(f'Word:\"{word}\" nachschlagen')
+    logging.debug(f'Word:\"{word}\" nachschlagen')
     page.locator('text=Zum vollst').nth(0).click()
     
-    print("get html with bs4")
+    logging.debug("get html with bs4")
     html = page.inner_html("div:has(div.division)")
-    import re
     soup = bs4.BeautifulSoup(html, "html.parser")
     bedeutung = soup.find_all("div", {"class":"enumeration__text"})
 
-    print("Getting definitions as list")
-    print("="*80)
+    logging.debug("Getting definitions as list")
+    logging.debug("="*80)
     
     definitions = []
     
@@ -97,17 +100,17 @@ def run(playwright: Playwright, csv_path:str, json_path:str, word:str) -> None:
         definitions.append((tag.text.strip("\n")))
     
     for count, definition in enumerate(definitions, start=1):
-        print(count, definition)
-        print("-"*80)
+        logging.debug(count, definition)
+        logging.debug("-"*80)
     
-    print("="*80)
+    logging.debug("="*80)
 
     export_definitions_as_csv(csv_path=csv_path, 
                             definitions=definitions,
                             word=word)
     store_html_as_json(page.url, json_path=json_path)
     
-    print("Closing browser and context")
+    logging.debug("Closing browser and context")
     context.close()
     browser.close()
 
